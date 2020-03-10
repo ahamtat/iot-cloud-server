557887a9e026799034cb5723583e1907556c14da
Artem Orlov <a.orlov@veedo.ru>
2020-03-10 15:02:44 +0300

    Added build script

---
+++ build/package/gocloudserver/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DirName=gocloudserver_$(date +%Y%m%d)
+
+# Build executable
+mkdir $DirName
+mkdir $DirName/cmd
+mkdir $DirName/cmd/gocloudserver
+cd $DirName/cmd/gocloudserver
+go build ../../../../../../cmd/gocloudserver/gocloudserver.go
+strip ./gocloudserver
+
+cd ../../..
+
+# Copy configure
+mkdir $DirName/configs
+cp ../../../configs/gocloudserver.yaml $DirName/configs
+
+# Make archive
+tar -zcvf $DirName.tar.gz $DirName
+
+# Delete temporary files
+rm -fR $DirName
