a7eb533415ff9cb5c5358788b64fe2db0b256a99
Artem Orlov <a.orlov@veedo.ru>
2020-03-24 11:11:50 +0300

    Added get configure request

--- internal/infrastructure/rest/server.go
+++ internal/infrastructure/rest/server.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/AcroManiac/iot-cloud-server/internal/infrastructure/broker"
+
 	"github.com/AcroManiac/iot-cloud-server/internal/infrastructure/logger"
 
 	"github.com/pkg/errors"
@@ -26,6 +28,7 @@ func init() {
 type Server struct {
 	router *gin.Engine
 	srv    *http.Server
+	mgr    *broker.Manager
 }
 
 // NewServer constructs REST server
@@ -56,7 +59,34 @@ func (s *Server) Init() error {
 	authorized.GET("/gateway/configure/:gatewayId", func(c *gin.Context) {
 		gatewayID := c.Param("gatewayId")
 		logger.Debug("Getting gateway configure", "gateway", gatewayID)
-		//c.JSON(http.StatusOK, configJSON)
+
+		// Create RPC request for gateway
+		request := &entities.IotMessage{
+			Timestamp:   entities.CreateTimestampMs(time.Now().Local()),
+			Vendor:      entities.VendorName,
+			Version:     entities.VeedoVersion,
+			GatewayId:   gatewayID,
+			ClientType:  "veedoCloud",
+			DeviceType:  "gateway",
+			Protocol:    "amqp",
+			MessageType: "configurationData",
+			Command:     "get",
+		}
+		response, err := s.mgr.DoGatewayRPC(gatewayID, request)
+		if err != nil {
+			errorText := "gateway RPC request failed"
+			logger.Error(errorText, "error", err, "gateway", gatewayID)
+			c.String(http.StatusBadRequest, errorText)
+			return
+		}
+		if response == nil {
+			errorText := "no gateway configuration returned"
+			logger.Error(errorText, "gateway", gatewayID)
+			c.String(http.StatusBadRequest, errorText)
+			return
+		}
+
+		c.String(http.StatusOK, string(response))
 	})
 
 	// Send command to gateway
@@ -67,7 +97,7 @@ func (s *Server) Init() error {
 	return nil
 }
 
-// Start RESTful server at all interfaces
+// Start RESTful server for all interfaces
 func (s *Server) Start() error {
 	addr := fmt.Sprintf(":%d", viper.GetInt("rest.port"))
 	s.srv = &http.Server{
